b9476857c89d8647955e92cd25614d471abf1a40
sliwaa <91088483+sliwaa@users.noreply.github.com>
2025-01-29 10:05:57 +0100

    Add files via upload

---
+++ client.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+# URL de votre API
+API_URL = "http://13.38.7.164"  # Remplacez par l'URL de votre API FastAPI
+
+@app.route("/")
+def index():
+    """Page d'index affichant la liste des étudiants."""
+    try:
+        response = requests.get(f"{API_URL}/etudiants")
+        response.raise_for_status()
+        etudiants = response.json()  # Récupère les données de l'API
+    except requests.exceptions.RequestException as e:
+        etudiants = []
+        print(f"Erreur lors de la récupération des étudiants : {e}")
+    return render_template("index.html", etudiants=etudiants)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="127.0.0.1", port=8000)
